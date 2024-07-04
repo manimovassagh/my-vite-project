7dcef574b1ecc0eb2f3b45870e375b5ba65c4ded
Mani Movassagh <mani.mgh@gmail.com>
2024-07-05 00:56:07 +0200

    remove webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,10 +13,7 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+
   ],
   reporter: [
     ['list'],
